861a6dac61367e8100556204dd8d10b9dfab3f7c
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-08 16:44:13 +0900

    init commit

---
+++ src/main/java/me/stone/rest/play/PlayRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.stone.rest.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/stone/rest/play/PlayRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.stone.rest.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
