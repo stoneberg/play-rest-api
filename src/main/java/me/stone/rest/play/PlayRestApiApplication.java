b2a0d348bbcec8bf188a05b3616e27a0c9166ca9
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-08 16:55:55 +0900

    second commit

--- src/main/java/me/stone/rest/play/PlayRestApiApplication.java
+++ src/main/java/me/stone/rest/play/PlayRestApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlayRestApiApplication {
 
+	// springboot rest-api app
 	public static void main(String[] args) {
 		SpringApplication.run(PlayRestApiApplication.class, args);
 	}
