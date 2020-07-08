55de5ec14c38ee9723fb1d6a68c962c4b1c9f226
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-09 01:15:14 +0900

    update data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into users (user_name, first_name, last_name, email, role, age, ssn) values ('stoneberg', 'Lee', 'YuPyeong', 'stoneberg@gmail.com', 'admin', '47', 'ssn-1134-3214');
-insert into users (user_name, first_name, last_name, email, role, age, ssn) values ('03president', 'Kim', 'YoungSam', 'youngsam@gmail.com', 'user', '88', 'ssn-5214-8521');
-insert into users (user_name, first_name, last_name, email, role, age, ssn) values ('hong2020', 'Hong', 'ManHee', 'hong2014@gmail.com', 'admin', '16', 'ssn-3652-6354');
+insert into users (user_name, first_name, last_name, email, role, age, ssn) values ('president03', 'Kim', 'YoungSam', 'president03@gmail.com', 'user', '88', 'ssn-5214-8521');
+insert into users (user_name, first_name, last_name, email, role, age, ssn) values ('hong2020', 'Hong', 'ManHee', 'hong2020@gmail.com', 'admin', '16', 'ssn-3652-6354');
 insert into users (user_name, first_name, last_name, email, role, age, ssn) values ('zetlee', 'Yoon', 'JaeHo', 'zetlee@gmail.com', 'admin', '35', 'ssn-6321-5421');
