780d6f2c34e7dd6b14c4b4c5561880e5f7025d61
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-09 15:00:30 +0900

    Exception handling using ResponseEntityExceptionHandler 2

--- src/main/java/me/stone/rest/play/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/me/stone/rest/play/exception/CustomGlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -22,6 +23,17 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
 			= new CustomErrorDetails(LocalDateTime.now(), "From MethodArgumentNotValid Exception in GEH", ex.getMessage());
 
 		return new ResponseEntity<>(customErrorDetails, HttpStatus.BAD_REQUEST);
+	}
+	
+	// HttpRequestMethodNotSupportedException
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+			HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		CustomErrorDetails customErrorDetails 
+			= new CustomErrorDetails(LocalDateTime.now(), "From HttpRequestMethodNotSupported Exception in GEH", ex.getMessage());
+		
+		return new ResponseEntity<>(customErrorDetails, HttpStatus.METHOD_NOT_ALLOWED);
 	}
 
 }
