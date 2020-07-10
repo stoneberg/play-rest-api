ddd237bf910b74054d9460161eb2382c39ba7c3d
Lee YuPyeong <stoneberg73@gmail.com>
2020-07-10 11:50:30 +0900

    Add Internalization

---
+++ src/main/java/me/stone/rest/play/config/PlayRestConfig.java
@@ -0,0 +1,31 @@
+package me.stone.rest.play.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@EnableWebMvc
+@Configuration
+public class PlayRestConfig {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:i18n/messages");
+		messageSource.setDefaultLocale(Locale.US);
+		messageSource.setCacheSeconds(180);
+		return messageSource;
+	}
+}
--- src/main/java/me/stone/rest/play/hello/controller/HelloController.java
+++ src/main/java/me/stone/rest/play/hello/controller/HelloController.java
@@ -1,6 +1,12 @@
 package me.stone.rest.play.hello.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.stone.rest.play.hello.payload.UserDetails;
@@ -8,14 +14,27 @@ import me.stone.rest.play.hello.payload.UserDetails;
 @RestController
 public class HelloController {
 	
+	@Autowired
+	private ReloadableResourceBundleMessageSource messageSource;
+
 	@GetMapping(path = "/helloworld")
 	public String helloWorld() {
 		return "Hello World1";
 	}
-	
-	@GetMapping(path="/helloworld-payload")
+
+	@GetMapping(path = "/helloworld-payload")
 	public UserDetails helloWorldPayload() {
 		return new UserDetails("Lee", "YuPyeong", "SeongNam");
+	}
+
+	@GetMapping("/i18n")
+	public String getMessage(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.welcome", null, new Locale(locale));
+	}
+	
+	@GetMapping("/i18n2")
+	public String getMessage2() {
+		return messageSource.getMessage("label.welcome", null, LocaleContextHolder.getLocale());
 	}
 
 }
